21114c068deafc5eedd58751dee2334095614c12
BSvetlana <svetlanabisacki@gmail.com>
2018-04-27 14:15:28 +0200

    "add contacts list"

---
+++ src/components/ContactList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="list-group">
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-success"
+                v-for="(contact,key) in contacts"
+                :key="key"><a href="#" style="color:#00796B; text-decoration: none"><b>{{contact.name}}</b></a></li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["contacts"]
+};
+</script>
+
+<style>
+
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
-    Contacts
+      <div class="container mt-4">
+          <div class="row">
+              <div class="col-4">
+                  <ContactList :contacts = 'contacts'/>
+              </div>
+              <div class="col-8">
+                  <ContactDetails/>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {};
+import ContactList from "../components/ContactList";
+import ContactDetails from "../components/ContactDetails";
+
+export default {
+  components: {
+    ContactList,
+    ContactDetails
+  },
+  data() {
+    return {
+      contacts: [
+        { name: "Branko" },
+        { name: "Marina" },
+        { name: "Sasa" },
+        { name: "Svetlana" }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
